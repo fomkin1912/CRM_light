21c32b74604cf1c7aba2ffb034d4903ef6f43a54
Lapin Viktor <fomkin1912@gmail.com>
2016-10-19 17:20:38 +0700

    add sorting on complex fields #18

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
 
-	def sortable(column, title = nil)
+	def sortable(column, title = nil, optional_columns = {})
     title ||= column.titleize
-   # css_class = column == sort_column ? "current #{sort_direction}" : nil
+    column = [column]
+   	column += optional_columns["columns"] if optional_columns.any?
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-		link_to title, { :sort => column, :direction => direction }
+  	link_to title, :sort => column, :direction => direction
 	end
+
 end
